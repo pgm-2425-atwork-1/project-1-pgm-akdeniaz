74d9c7729ace46b1608962b558c191d181c53bf8
pgm-akdeniaz <akdenis.niaziev@student.arteveldehs.be>
2024-12-10 09:51:40 +0100

    javascript click-events

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,16 @@
-console.log("test");
+const $hamburger = document.getElementById("hamburger");
+const $dropdown = document.getElementById("dropdown");
+const $button = document.getElementById("dropdown-button");
+const $main = document.getElementById("main");
+const $filler = document.getElementById("filler");
+
+function toggleDropdown(state) {
+  $dropdown.classList.toggle("toggle", state);
+  $filler.classList.toggle("filler--toggle", state);
+}
+
+$hamburger.addEventListener("click", () => toggleDropdown());
+$button.addEventListener("click", () => toggleDropdown(false));
+$main.addEventListener("click", (event) => {
+  if (event.target !== $dropdown) toggleDropdown(false);
+});
